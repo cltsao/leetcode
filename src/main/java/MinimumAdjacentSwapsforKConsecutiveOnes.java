aeeadd8b1b2d2ac05bde155e42c186f8ab59db96
Cheng-Lin Tsao <chenglin.tsao@gmail.com>
2022-02-02 16:28:55 -0500

    1703. Minimum Adjacent Swaps for K Consecutive Ones using sliding window

--- src/main/java/MinimumAdjacentSwapsforKConsecutiveOnes.java
+++ src/main/java/MinimumAdjacentSwapsforKConsecutiveOnes.java
@@ -1,6 +1,42 @@
 import java.util.ArrayList;
 
 public class MinimumAdjacentSwapsforKConsecutiveOnes {
+    // Assume min swap is achieved by moving toward the median index. Using a sliding window, time complexity: O(N)
+    public int minMoves(int[] nums, int k) {
+        long startTime = System.currentTimeMillis();
+
+        ArrayList<Integer> onePositions = new ArrayList<>();
+        for(int pos = 0; pos < nums.length; ++pos) {
+            if (nums[pos] == 1)
+                onePositions.add(pos);
+        }
+
+        int medianPosIndex = (k + 1) / 2 - 1;
+        int preRadius = medianPosIndex;
+        int preSum = 0;
+        for(int i = 0; i < medianPosIndex; ++i) {
+            preSum += onePositions.get(i);
+        }
+        int postRadius = k - 1 - medianPosIndex;
+        int postSum = 0;
+        for(int i = medianPosIndex + 1; i < k; ++i) {
+            postSum += onePositions.get(i);
+        }
+        int minMoves = (k % 2 == 1) ? (postSum - preSum - preRadius * (preRadius + 1)) : (postSum - preSum - onePositions.get(medianPosIndex) - (preRadius + 1) * (preRadius + 1));
+
+        while (medianPosIndex + postRadius < onePositions.size() - 1) {
+            preSum -= onePositions.get(medianPosIndex - preRadius);
+            preSum += onePositions.get(medianPosIndex);
+            postSum -= onePositions.get(medianPosIndex + 1);
+            postSum += onePositions.get(medianPosIndex + postRadius + 1);
+            ++medianPosIndex;
+            minMoves = Math.min(minMoves, (k % 2 == 1) ? (postSum - preSum - preRadius * (preRadius + 1)) : (postSum - preSum - onePositions.get(medianPosIndex) - (preRadius + 1) * (preRadius + 1)));
+        }
+
+        System.out.println("Execution time: " + (System.currentTimeMillis() - startTime) + " ms");
+        return minMoves;
+    }
+
     int getMoves(ArrayList<Integer> onePositions, int positionIndex, int k) {
         int headPosition = onePositions.get(positionIndex);
         int tailPosition = onePositions.get(positionIndex);
@@ -24,7 +60,7 @@ public class MinimumAdjacentSwapsforKConsecutiveOnes {
     }
 
     // Anchor each one and calculate number of swaps using greedy algorithm. Time complexity: O(N * K)
-    public int minMoves(int[] nums, int k) {
+    public int minMovesGreedy(int[] nums, int k) {
         long startTime = System.currentTimeMillis();
 
         ArrayList<Integer> onePositions = new ArrayList<>();
